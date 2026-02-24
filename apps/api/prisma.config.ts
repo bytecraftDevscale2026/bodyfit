5bb93fa0e7a12ed942b52a5d5c449939ee4e0ec8
AnnisaFarikha <nisa.43325003@mhs.polines.ac.id>
2026-02-24 13:35:54 +0700

    update fixed

--- apps/api/prisma.config.ts
+++ apps/api/prisma.config.ts
@@ -2,15 +2,15 @@ import { config } from "dotenv";
 import { defineConfig } from "prisma/config";
 
 config({
-  path: "../../.env",
+	path: "../../.env",
 });
 
 export default defineConfig({
-  schema: "prisma/schema.prisma",
-  migrations: {
-    path: "prisma/migrations",
-  },
-  datasource: {
-    url: process.env.DATABASE_URL!,
-  },
+	schema: "prisma/schema.prisma",
+	migrations: {
+		path: "prisma/migrations",
+	},
+	datasource: {
+		url: process.env.DATABASE_URL!,
+	},
 });
